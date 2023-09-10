093651e4a74ab50469cdc8e0cbd55593d7f5e7a5
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-09-10 23:26:25 +0300

    Add starter files

---
+++ src/main/java/com/danieljhv/tasktemple/TaskTempleApplication.java
@@ -0,0 +1,13 @@
+package com.danieljhv.tasktemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTempleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieljhv/tasktemple/TaskTempleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieljhv.tasktemple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTempleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
