cbfc9d73f95512dcea37e18aee6d1e2f4f27f84b
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-09-11 13:27:02 +0300

    Add task entity

---
+++ src/main/java/com/danieljhv/tasktemple/entity/Task.java
@@ -0,0 +1,60 @@
+package com.danieljhv.tasktemple.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(long id, String title, String description, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
