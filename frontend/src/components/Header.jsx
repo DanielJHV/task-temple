6aa566b0fe20d7cbc0afb4528b9627ab41c1c99d
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-09-12 12:44:38 +0300

    Add Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <a className="header__link" href="/">
+        <div className="logo-box">
+          <img className="logo" src="src/assets/logo.png" />
+        </div>
+      </a>
+      {/* <Nav /> */}
+    </header>
+  );
+}
+
+export default Header;
