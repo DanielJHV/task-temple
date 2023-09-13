b2f7debf3a2fef697ec94a96f3e3989fb0e86364
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-09-13 14:08:20 +0300

    Add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
 import TasksList from "./components/TasksList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <TasksList />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<TasksList />}></Route>
+          <Route path="/tasks" element={<TasksList />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
